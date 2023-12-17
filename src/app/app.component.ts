fbaccce288c62a1421b6d12bf0b83ef08e2b5d99
Anselmo <ansinftec17@gmail.com>
2023-12-17 09:27:33 -0300

    FEAT:Adicionando galeria de itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Itens } from './models/item.models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loja-jordan';
+
+
+  public list: Itens[] = [
+    {
+      title: 'Air Jordan 1 Mid Dutch Green',
+      marca: 'Tênis Air Jordan',
+      preco: '1.199,99',
+      image: 'air-jordan-1-mid-dutch-green-1-400.svg'
+    },
+    {
+      title: 'Air Jordan 1 Mid Dutch Green',
+      marca: 'Tênis Air Jordan',
+      preco: '1.049,99',
+      image: 'air-jordan-1-high-zoom-cmft-tropical-twist-1-400.svg'
+    },
+    {
+      title: 'Air Jordan 1 Mid Dutch Green',
+      marca: 'Tênis Air Jordan',
+      preco: '1.350,00',
+      image: 'air-jordan-1-retro-high-court-purple-w-1-400.svg'
+    },
+    {
+      title: 'Air Jordan 1 Mid GS Light Smoke Grey',
+      marca: 'Tênis Air Jordan',
+      preco: '1.585,00',
+      image: 'air-jordan-1-mid-light-smoke-grey-gs-1-1000.svg'
+    },
+    {
+      title: 'Air Jordan 1 Mid SE Brigth Citrus',
+      marca: 'Tênis Air Jordan',
+      preco: '949,99',
+      image: 'air-jordan-1-mid-bright-citrus-1-1000 1.svg'
+    },
+    {
+      title: 'Air Jordan 1 Mid Grey Camo',
+      marca: 'Tênis Air Jordan',
+      preco: '999,99',
+      image: 'air-jordan-1-mid-grey-camo-1-1000 1.svg'
+    }
+  ]
 }
---
+++ src/app/models/item.models.ts
@@ -0,0 +1,6 @@
+export interface Itens {
+    title: string;
+    marca: string;
+    preco: string;
+    image:string;
+}
